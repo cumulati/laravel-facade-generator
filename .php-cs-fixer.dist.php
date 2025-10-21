25f75995f7cb1ea8db9b6c6041391810150e32b6
Patrick McCarren <patrick@cumulative.io>
2025-10-21 12:48:53 -0500

    fix: php-cs-fixer namespace blank lines

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -39,7 +39,6 @@ return $config
 		'no_whitespace_before_comma_in_array' => true,
 		'no_whitespace_in_blank_line' => true,
 		'object_operator_without_whitespace' => true,
-		'single_blank_line_before_namespace' => true,
 		'space_after_semicolon' => true,
 		'ternary_operator_spaces' => true,
 		'trim_array_spaces' => true,
